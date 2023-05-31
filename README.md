9983275dce89b8e48145db258fea93dfbda42a5d
febin123 <febsep96@gmail.com>
2023-06-01 00:35:21 +0530

    Namaste React-2

---
+++ README.md
@@ -0,0 +1,16 @@
+# Parcel
+
+-Development Build
+-Local server
+-HMR= Hot Module Replacement
+-File Watching alogrithm - written in C++
+-Caching- Faster Builds 
+-Image optimization
+-Minification
+-Bundling
+-compress the file
+-differential bundling=operate on older browsers
+-Error handling
+-HTTPS
+-Tree shaking=removes unwanted/unused code from the code
+
---
+++ app.js
@@ -0,0 +1,12 @@
+import React from "react"
+import ReactDOM from "react-dom/client"
+const main3=React.createElement('h1',{},"heading-1")
+const main4=React.createElement('h1',{},"heading-2")
+const main2=React.createElement('div',{id:'child'},main3,main4)
+const main=React.createElement('div',{id:'parent'},main2)
+
+const roott=ReactDOM.createRoot(document.getElementById('root'))
+// heading2.innerText='This is heading2'
+roott.render(main)
+
+// console.log(heading2, roott)
